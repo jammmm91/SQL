57d2752aa0b7a9fcde53d1fae367050d3d7a85a4
jammmm91 <ganjinam00@nave.com>
2021-04-09 19:43:16 +0900

    new

---
+++ 0402.sql
@@ -0,0 +1,9 @@
+-- DDL (Data Definition Language) 데이터 정의어
+   CREATE(테이블생성), ARTER(테이블을 변화), DROP(테이블 삭제)
+-- DML (Data Manipulation Language) 데이터 조작어
+   UPDATE DELETE INSERT 데이터를 조작하는
+        --값을 최신화, 삭제, 추가
+-- DCL (Data Control Language) 데이터 제어어
+   GRANT, REVOKE
+-- SELECT
+   SELECT (조회) 
